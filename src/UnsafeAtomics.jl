6fea5500349db96e794395e9997bd6e997672abd
Takafumi Arakaki <aka.tkf@gmail.com>
2021-09-25 21:08:46 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/UnsafeAtomics.jl
@@ -0,0 +1,11 @@
+baremodule UnsafeAtomics
+
+module Internal
+
+using ..UnsafeAtomics: UnsafeAtomics
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule UnsafeAtomics
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using UnsafeAtomics
+using Test
+
+@testset "UnsafeAtomics.jl" begin
+    # Write your tests here.
+end
