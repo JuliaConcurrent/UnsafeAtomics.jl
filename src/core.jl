0d1bda817b5216545524501dfbd2fd1768f7036d
Takafumi Arakaki <aka.tkf@gmail.com>
2022-06-14 10:52:12 -0400

    Simplify fallback `cas!`

--- src/core.jl
+++ src/core.jl
@@ -175,52 +175,13 @@ function UnsafeAtomics.cas!(
     success_ordering,
     failure_ordering,
 ) where {T}
-    if sizeof(T) == 1
-        (old, success) = UnsafeAtomics.cas!(
-            Ptr{UInt8}(x),
-            bitcast(UInt8, cmp),
-            bitcast(UInt8, new),
-            success_ordering,
-            failure_ordering,
-        )
-        return (old = bitcast(T, old), success = success)
-    elseif sizeof(T) == 2
-        (old, success) = UnsafeAtomics.cas!(
-            Ptr{UInt16}(x),
-            bitcast(UInt16, cmp),
-            bitcast(UInt16, new),
-            success_ordering,
-            failure_ordering,
-        )
-        return (old = bitcast(T, old), success = success)
-    elseif sizeof(T) == 4
-        (old, success) = UnsafeAtomics.cas!(
-            Ptr{UInt32}(x),
-            bitcast(UInt32, cmp),
-            bitcast(UInt32, new),
-            success_ordering,
-            failure_ordering,
-        )
-        return (old = bitcast(T, old), success = success)
-    elseif sizeof(T) == 8
-        (old, success) = UnsafeAtomics.cas!(
-            Ptr{UInt64}(x),
-            bitcast(UInt64, cmp),
-            bitcast(UInt64, new),
-            success_ordering,
-            failure_ordering,
-        )
-        return (old = bitcast(T, old), success = success)
-    elseif sizeof(T) == 16
-        (old, success) = UnsafeAtomics.cas!(
-            Ptr{UInt128}(x),
-            bitcast(UInt128, cmp),
-            bitcast(UInt128, new),
-            success_ordering,
-            failure_ordering,
-        )
-        return (old = bitcast(T, old), success = success)
-    else
-        error(LazyString("unsupported size: ", sizeof(T)))
-    end
+    UI = as_native_uint(T)
+    (old, success) = UnsafeAtomics.cas!(
+        Ptr{UI}(x),
+        bitcast(UI, cmp),
+        bitcast(UI, new),
+        success_ordering,
+        failure_ordering,
+    )
+    return (old = bitcast(T, old), success = success)
 end
