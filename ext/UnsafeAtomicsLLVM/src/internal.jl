091d4b7cab56ed8d41700ab8aa02062588ff36ff
Valentin Churavy <vchuravy@users.noreply.github.com>
2023-03-27 10:54:08 -0400

    Move Atomics from LLVM.jl into UnsafeAtomicsLLVM (JuliaConcurrent/UnsafeAtomicsLLVM.jl#8)

--- ext/UnsafeAtomicsLLVM/src/internal.jl
+++ ext/UnsafeAtomicsLLVM/src/internal.jl
@@ -4,19 +4,21 @@ julia_ordering_name(::typeof(UnsafeAtomics.acquire_release)) = :acquire_release
 julia_ordering_name(::typeof(UnsafeAtomics.sequentially_consistent)) =
     :sequentially_consistent
 
+include("atomics.jl")
+
 @inline UnsafeAtomics.load(ptr::LLVMPtr, order::Ordering) =
-    LLVM.Interop.atomic_pointerref(ptr, Val{julia_ordering_name(order)}())
+    atomic_pointerref(ptr, Val{julia_ordering_name(order)}())
 
 @inline function UnsafeAtomics.store!(ptr::LLVMPtr, x, order::Ordering)
-    LLVM.Interop.atomic_pointerset(ptr, x, Val{julia_ordering_name(order)}())
+    atomic_pointerset(ptr, x, Val{julia_ordering_name(order)}())
     return
 end
 
 mapop(op::OP) where {OP} = op
-mapop(::typeof(UnsafeAtomics.right)) = LLVM.Interop.right
+mapop(::typeof(UnsafeAtomics.right)) = right
 
 @inline UnsafeAtomics.modify!(ptr::LLVMPtr, op::OP, x, order::Ordering) where {OP} =
-    LLVM.Interop.atomic_pointermodify(ptr, mapop(op), x, Val{julia_ordering_name(order)}())
+    atomic_pointermodify(ptr, mapop(op), x, Val{julia_ordering_name(order)}())
 
 @inline UnsafeAtomics.cas!(
     ptr::LLVMPtr,
@@ -24,7 +26,7 @@ mapop(::typeof(UnsafeAtomics.right)) = LLVM.Interop.right
     desired,
     success_order::Ordering,
     failure_order::Ordering,
-) = LLVM.Interop.atomic_pointerreplace(
+) = atomic_pointerreplace(
     ptr,
     expected,
     desired,
