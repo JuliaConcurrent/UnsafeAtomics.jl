f2f936457c177da6e9fdcff0afec3763408e89cd
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2024-04-04 11:51:31 -0700

    Use atomic_pointer* methods when available

--- src/core.jl
+++ src/core.jl
@@ -56,35 +56,49 @@ for typ in (inttypes..., floattypes...)
     for ord in orderings
         ord in (release, acq_rel) && continue
 
-        @eval function UnsafeAtomics.load(x::Ptr{$typ}, ::$(typeof(ord)))
-            return llvmcall(
-                $("""
-                %ptr = inttoptr i$WORD_SIZE %0 to $lt*
-                %rv = load atomic $rt %ptr $ord, align $(sizeof(typ))
-                ret $lt %rv
-                """),
-                $typ,
-                Tuple{Ptr{$typ}},
-                x,
-            )
+        if isdefined(Core.Intrinsics, :atomic_pointerref)
+            @eval function UnsafeAtomics.load(x::Ptr{$typ}, ::$(typeof(ord)))
+                base_ord = llvm_ordering($ord)
+                return Core.Intrinsics.atomic_pointerref(x, base_ord)
+            end
+        else
+            @eval function UnsafeAtomics.load(x::Ptr{$typ}, ::$(typeof(ord)))
+                return llvmcall(
+                    $("""
+                    %ptr = inttoptr i$WORD_SIZE %0 to $lt*
+                    %rv = load atomic $rt %ptr $ord, align $(sizeof(typ))
+                    ret $lt %rv
+                    """),
+                    $typ,
+                    Tuple{Ptr{$typ}},
+                    x,
+                )
+            end
         end
     end
 
     for ord in orderings
         ord in (acquire, acq_rel) && continue
 
-        @eval function UnsafeAtomics.store!(x::Ptr{$typ}, v::$typ, ::$(typeof(ord)))
-            return llvmcall(
-                $("""
-                %ptr = inttoptr i$WORD_SIZE %0 to $lt*
-                store atomic $lt %1, $lt* %ptr $ord, align $(sizeof(typ))
-                ret void
-                """),
-                Cvoid,
-                Tuple{Ptr{$typ},$typ},
-                x,
-                v,
-            )
+        if isdefined(Core.Intrinsics, :atomic_pointerset)
+            @eval function UnsafeAtomics.store!(x::Ptr{$typ}, v::$typ, ::$(typeof(ord)))
+                base_ord = llvm_ordering($ord)
+                return Core.Intrinsics.atomic_pointerset(x, v, base_ord)
+            end
+        else
+            @eval function UnsafeAtomics.store!(x::Ptr{$typ}, v::$typ, ::$(typeof(ord)))
+                return llvmcall(
+                    $("""
+                    %ptr = inttoptr i$WORD_SIZE %0 to $lt*
+                    store atomic $lt %1, $lt* %ptr $ord, align $(sizeof(typ))
+                    ret void
+                    """),
+                    Cvoid,
+                    Tuple{Ptr{$typ},$typ},
+                    x,
+                    v,
+                )
+            end
         end
     end
 
@@ -93,37 +107,57 @@ for typ in (inttypes..., floattypes...)
 
         typ <: AbstractFloat && break
 
-        @eval function UnsafeAtomics.cas!(
-            x::Ptr{$typ},
-            cmp::$typ,
-            new::$typ,
-            ::$(typeof(success_ordering)),
-            ::$(typeof(failure_ordering)),
-        )
-            success = Ref{Int8}()
-            GC.@preserve success begin
-                old = llvmcall(
-                    $(
-                        """
-                        %ptr = inttoptr i$WORD_SIZE %0 to $lt*
-                        %rs = cmpxchg $lt* %ptr, $lt %1, $lt %2 $success_ordering $failure_ordering
-                        %rv = extractvalue { $lt, i1 } %rs, 0
-                        %s1 = extractvalue { $lt, i1 } %rs, 1
-                        %s8 = zext i1 %s1 to i8
-                        %sptr = inttoptr i$WORD_SIZE %3 to i8*
-                        store i8 %s8, i8* %sptr
-                        ret $lt %rv
-                        """
-                    ),
-                    $typ,
-                    Tuple{Ptr{$typ},$typ,$typ,Ptr{Int8}},
+        if isdefined(Core.Intrinsics, :atomic_pointerreplace)
+            @eval function UnsafeAtomics.cas!(
+                x::Ptr{$typ},
+                cmp::$typ,
+                new::$typ,
+                ::$(typeof(success_ordering)),
+                ::$(typeof(failure_ordering)),
+            )
+                base_success_ordering = llvm_ordering($success_ordering)
+                base_failure_ordering = llvm_ordering($failure_ordering)
+                return Core.Intrinsics.atomic_pointerreplace(
                     x,
                     cmp,
                     new,
-                    Ptr{Int8}(pointer_from_objref(success)),
+                    base_success_ordering,
+                    base_failure_ordering
                 )
             end
-            return (old = old, success = !iszero(success[]))
+        else
+            @eval function UnsafeAtomics.cas!(
+                x::Ptr{$typ},
+                cmp::$typ,
+                new::$typ,
+                ::$(typeof(success_ordering)),
+                ::$(typeof(failure_ordering)),
+            )
+                success = Ref{Int8}()
+                GC.@preserve success begin
+                    old = llvmcall(
+                        $(
+                            """
+                            %ptr = inttoptr i$WORD_SIZE %0 to $lt*
+                            %rs = cmpxchg $lt* %ptr, $lt %1, $lt %2 $success_ordering $failure_ordering
+                            %rv = extractvalue { $lt, i1 } %rs, 0
+                            %s1 = extractvalue { $lt, i1 } %rs, 1
+                            %s8 = zext i1 %s1 to i8
+                            %sptr = inttoptr i$WORD_SIZE %3 to i8*
+                            store i8 %s8, i8* %sptr
+                            ret $lt %rv
+                            """
+                        ),
+                        $typ,
+                        Tuple{Ptr{$typ},$typ,$typ,Ptr{Int8}},
+                        x,
+                        cmp,
+                        new,
+                        Ptr{Int8}(pointer_from_objref(success)),
+                    )
+                end
+                return (old = old, success = !iszero(success[]))
+            end
         end
     end
 
