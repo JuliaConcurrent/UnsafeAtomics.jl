6831593b81330972d119dc6e234f06e16785585e
Tim Besard <tim.besard@gmail.com>
2026-09-25 14:25:47 +0200

    Declare the API public.
    Nothing is exported, so mark the functions, orderings and scopes as
    `public` on Julia 1.11 and later, to distinguish them from the internals
    that back-ends currently reach into.

--- src/UnsafeAtomics.jl
+++ src/UnsafeAtomics.jl
@@ -55,4 +55,14 @@ const workgroup = Internal.workgroup
 const device = Internal.device
 const system = Internal.system
 
+Internal.declare_public(
+    UnsafeAtomics,
+    :Ordering, :SyncScope,
+    :load, :store!, :cas!, :modify!, :fence,
+    :add!, :sub!, :xchg!, :and!, :nand!, :or!, :xor!, :max!, :min!, :right,
+    :unordered, :monotonic, :acquire, :release, :acq_rel, :seq_cst,
+    :acquire_release, :sequentially_consistent,
+    :none, :singlethread, :subgroup, :workgroup, :device, :system,
+)
+
 end  # baremodule UnsafeAtomics
